4e0b5fcade9a7a71e7d27fafcc2efef12057feb0
yanataranets <yana.taranets.s@gmail.com>
2021-09-20 12:22:42 +0200

    api for commmetns

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Comment;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
 use App\Post;
 use App\Repository\Post\PostInterface;
@@ -12,7 +13,7 @@ use App\Repository\Comment\CommentInterface;
 use function is_null;
 use function redirect;
 
-class CommentController extends Controller
+class CommentController extends BaseController
 {
     //
     protected $comment;
